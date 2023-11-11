846f24a81172fa3fd9a45f4b6762c4f09263f03c
Erica M. ("Loonatic") <l.oony@aol.com>
2023-11-10 19:53:52 -0600

    Final Project: Refactor how other csv files are parsed, fix up partOne

--- final_project/PartOne.py
+++ final_project/PartOne.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
+import csv
 
-extension = ".csv"
+ext = ".csv"
 
 
 @dataclass
@@ -14,46 +15,79 @@ class Student:
     graduation_date: str = ""
 
 
+allRegisteredStudents = []
 
-students = []
-import csv
-
-# Open file
-with open('StudentsMajorsList-3.csv') as file_obj:
-
-    # Create reader object by passing the file
-    # object to reader method
-    reader_obj = csv.reader(file_obj)
-
-    # Iterate over each row in the csv
-    # file using reader object
-    for row in reader_obj:
+# Open our file w/ all our students and parse data into a Student object
+with open(f'StudentsMajorsList-3{ext}') as csvFile:
+    csvReader = csv.reader(csvFile)
+    # Our disciplinary entries are half-baked. For ones who don't have any, create a entry with "N"
+    # So that the sizes of our rows are balanced.
+    for row in csvReader:
         if row[-1] == "":
             row[-1] = "N"
         if row[-1] != "Y" and row[-1] != "N":
             row.append('N')
         student = Student(*row)
-        students.append(student)
+        allRegisteredStudents.append(student)
+
+# We're not done registering our students just yet -- we need to open up two more CSV files
+# that hold graduation dates & GPA.
+
+# Student.id : Student
+id2student = dict()
+
+for student in allRegisteredStudents:
+    id2student[student.student_id] = student
+
+# First, we need to register the graduation dates for the students.
+with open(f'GraduationDatesList-1{ext}') as csvFile:
+    csvReader = csv.reader(csvFile)
+    for row in csvReader:
+        studentId, gdate = row
+        studentEntry = id2student.get(studentId)
+        if studentEntry:
+            studentEntry.graduation_date = gdate
+
+# Now we need to register the GPAs.
+with open(f'GPAList-1{ext}') as csvFile:
+    csvReader = csv.reader(csvFile)
+    for row in csvReader:
+        studentId, gpa = row
+        studentEntry = id2student.get(studentId)
+        if studentEntry:
+            studentEntry.gpa = gpa
 
 
 def partOne():
     """
-    The items should
-    be sorted alphabetically by student last name. Each row should contain student ID,
-    major, first name, last name, GPA, graduation date and indicate if disciplinary action was
-    taken. The student attributes must appear in this order in each row
+    The items should be sorted alphabetically by student last name.
+    The student attributes must appear in this order in each row:
+        Student ID, major, first name, last name, GPA, graduation date, disciplinary action taken
     """
     lastname2student = dict()
-    lastname2student_sorted = dict()
-    for student in students:
+    for student in allRegisteredStudents:
         lastname2student[student.last_name] = student
 
-    for entry in sorted(lastname2student.keys()):
-        studentInfo = lastname2student[entry]
-        lastname2student_sorted[studentInfo.last_name] = studentInfo
+    csvFilename = f"FullRoster{ext}"
+    csvFileOut = open(csvFilename, 'w')
+    csvwriter = csv.writer(csvFileOut)
 
-    print(lastname2student_sorted)
+    # Write header
+    csvwriter.writerow([
+        "Student ID", "Major", "First Name", "Last Name", "GPA", "Graduation Date", "Disciplinary Action Taken"
+    ])
 
+    for lastName in sorted(lastname2student.keys()):
+        studentInfo = lastname2student[lastName]
+        studentId = studentInfo.student_id
+        studentMajor = studentInfo.student_major
+        firstName = studentInfo.first_name
+        studentGpa = studentInfo.gpa
+        gradDate = studentInfo.graduation_date
+        dpAction = studentInfo.disciplinary
+        csvwriter.writerow([studentId, studentMajor, firstName, lastName, studentGpa, gradDate, dpAction])
+
+    csvFileOut.close()
 
 def partTwo():
     """
@@ -76,3 +110,4 @@ def partThree():
 
 
 partOne()
+partTwo()
